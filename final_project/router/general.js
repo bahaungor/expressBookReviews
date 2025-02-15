ae29c5ff8b9604230cf03dfc2bb1ddc784fcdd2c
bahaungor <bahadirungor@gmail.com>
2025-02-15 17:43:36 -0500

    async implementation completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let books = require("./booksdb.js");
+let bookspromise = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -15,19 +15,17 @@ public_users.post("/register", (req,res) => {
     users.push({username, password})
     return res.status(201).json({message: "User created"});
   }
-  
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-//   console.log(books)
+public_users.get('/',async function (req, res) {
+    const books = await bookspromise
     res.json(books)
-
-//   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
+    const books = await bookspromise
     const book_nums = Object.keys(books)
     const isbn = req.params.isbn;
     if (book_nums.includes(`${isbn}`)){
@@ -36,9 +34,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
         res.status(404).json({message: 'Invalid book number'})
     }
  });
-  
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
+  const books = await bookspromise
   const book_authors = Object.values(books).map(book => book.author)
   const author = req.params.author;
   if (book_authors.includes(author)){
@@ -50,7 +49,8 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
+    const books = await bookspromise
     const book_titles = Object.values(books).map(book => book.title)
     const title = req.params.title;
     if (book_titles.includes(title)){
